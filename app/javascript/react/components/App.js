283c97c9c604c4e03a97d72dd09567c39cb0de91
Stephen Fang <safang99@gmail.com>
2019-10-28 15:43:57 -0400

    staged with clear and set buttons

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
 export const App = (props) => {
-  var board1 = ChessBoard('board1', 'start')
+
+  $(function() {
+    var board2 = Chessboard('board2', {
+    draggable: true,
+    pieceTheme: 'https://koblenski.github.io/javascript/chessboardjs-0.3.0/img/chesspieces/wikipedia/{piece}.png',
+    dropOffBoard: 'trash',
+    sparePieces: true,
+    position: 'start'
+  })
+debugger
+  $('#startBtn').on('click', board2.start)
+  $('#clearBtn').on('click', board2.clear)
+})
   return (<h1>Make It So React</h1>)
 }
 
